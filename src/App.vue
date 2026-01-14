37361b3ca54683c142d433a9c955ef2e374d507e
Lu Wilson <lu@todepond.com>
2026-01-14 18:08:45 +0000

    clean up CSS

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,8 @@
 <script setup>
 import { RouterView } from 'vue-router';
+import './assets/main.css';
 </script>
 
 <template>
   <RouterView />
 </template>
-
-<style scoped>
-nav {
-  display: flex;
-  gap: 1rem;
-}
-</style>
