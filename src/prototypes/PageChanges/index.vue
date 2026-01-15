95bd58a6d70f558e84369c444161249306b352f2
Lu Wilson <lu@todepond.com>
2026-01-15 14:44:13 +0000

    add PageChanges prototype

--- src/prototypes/PageChanges/index.vue
+++ src/prototypes/PageChanges/index.vue
@@ -1,39 +1,64 @@
 <script setup>
-import { CdxButton, CdxCard, CdxLabel, CdxProgressIndicator, CdxTextInput } from '@wikimedia/codex';
+import { CdxButton, CdxLabel, CdxProgressIndicator, CdxTextInput } from '@wikimedia/codex';
 import { onMounted, ref } from 'vue';
 import { WikiApi } from '../../wiki';
 
 const wiki = new WikiApi();
 
-const url = ref('');
-const title = ref('');
-const description = ref('');
-const supportingText = ref('');
-const thumbnail = ref(null);
-const searchQuery = ref('Wet Leg');
+const searchQuery = ref(sessionStorage.getItem('searchQuery') || 'Wet Leg');
+const history = ref([]);
 const isLoading = ref(false);
-const search = async () => {
-  const summary = await wiki.getPageHistory(searchQuery.value);
-  isLoading.value = false;
-  console.log(summary);
-
-  url.value = summary.content_urls.desktop.page;
-  title.value = summary.title;
-  description.value = summary.description;
-  supportingText.value = summary.extract;
-  thumbnail.value = summary.thumbnail?.source
-    ? {
-        url: summary.thumbnail.source,
-      }
-    : null;
-};
 
 onMounted(search);
+
+function saveSearchQuery(query) {
+  sessionStorage.setItem('searchQuery', query);
+}
+
+async function search() {
+  isLoading.value = true;
+  const _history = await wiki.getPageHistory(searchQuery.value);
+  isLoading.value = false;
+  console.log(_history);
+  history.value = _history;
+  saveSearchQuery(searchQuery.value);
+}
+
+function formatTimestamp(timestamp) {
+  const date = new Date(timestamp);
+  const dateString = date.toLocaleDateString('en-GB', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
+  const timeString = date.toLocaleTimeString('en-GB', {
+    hour: '2-digit',
+    minute: '2-digit',
+  });
+  return `${timeString}, ${dateString}`;
+}
+
+function getDeltaClass(delta) {
+  if (delta > 0) {
+    return 'positive';
+  } else if (delta < 0) {
+    return 'negative';
+  } else {
+    return 'neutral';
+  }
+}
+
+function getUserUrl(user) {
+  return `https://en.wikipedia.org/wiki/${user.name}`;
+}
+
+function getRevisionUrl(id) {
+  return `https://en.wikipedia.org/w/index.php?title=${searchQuery.value}&diff=${id}`;
+}
 </script>
 
 <template>
-  <section>
-    <p><em>This page is under construction and doesn't work yet.</em></p>
+  <main>
     <form @submit.prevent="search">
       <CdxLabel input-id="page-name">Page name</CdxLabel>
 
@@ -43,19 +68,56 @@ onMounted(search);
         <CdxProgressIndicator v-if="isLoading" aria-label="Loading page" />
       </span>
     </form>
-    <CdxCard :thumbnail="thumbnail" :url="url">
-      <template #title>{{ title }}</template>
-      <template #description>{{ description }}</template>
-      <template #supporting-text>{{ supportingText }}</template>
-    </CdxCard>
-  </section>
+    <section class="changes">
+      <div class="change" v-for="change in history.revisions" :key="change.timestamp">
+        <p>
+          <a :href="getRevisionUrl(change.id)">{{ change.comment }}</a>
+        </p>
+        <p>
+          <a :href="getUserUrl(change.user)">
+            <strong>{{ change.user.name }}</strong>
+          </a>
+          &nbsp;<span :class="getDeltaClass(change.delta)">{{ change.delta }}</span>
+        </p>
+        <p>
+          <span>{{ formatTimestamp(change.timestamp) }}</span>
+        </p>
+      </div>
+    </section>
+  </main>
 </template>
 
 <style scoped>
-section {
+.changes {
+  margin: 0.5rem 0;
   display: flex;
   flex-direction: column;
-  gap: 1rem;
+  gap: 0.5rem;
+}
+
+.changes p {
+  margin: 0;
+}
+
+.change {
+  border: 1px solid var(--border-color-base);
+  padding: 0.5rem;
+}
+
+.positive {
+  color: var(--color-content-added);
+}
+
+.positive::before {
+  content: '+';
+}
+
+.negative {
+  color: var(--color-content-removed);
+}
+
+.neutral {
+  color: var(--color-base);
 }
 
 .cdx-text-input {
